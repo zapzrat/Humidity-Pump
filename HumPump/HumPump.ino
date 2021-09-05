ca6cefe7fb583aab2181b7356ad93eab9b5f1059
zapzrat <zapzrat@gmail.com>
2021-09-06 03:47:13 +0700

    Add files via upload

---
+++ HumPump/HumPump.ino
@@ -0,0 +1,24 @@
+int pinRelay = 13;
+int humidity = 0;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Micro Projects TH");
+
+  pinMode(pinRelay, OUTPUT);
+}
+
+void loop() {
+  humidity = analogRead(A0);
+  Serial.print("humidity : "); Serial.println(humidity);
+
+  if (humidity > 700) {
+    digitalWrite(pinRelay, LOW);
+  }
+  else if (humidity <= 700) {
+
+    digitalWrite(pinRelay, HIGH);
+  }
+
+  delay(1000);
+}
